58095d302f8330dda41612860837dcd19231e90b
manikandanxenon <manikandan@wearexenon.com>
2017-07-21 14:11:46 +0530

    iterative binary search

--- search/binary/binary.js
+++ search/binary/binary.js
@@ -2,7 +2,7 @@
 
 var list = [1, 4, 7, 9];
 
-const binarySearch = (data, from, to, value) => {
+const recursiveBinarySearch = (data, from, to, value) => {
     if (to >= from) {
         var mid = Math.floor(from + (to - from) / 2);
         if (data[mid] === value) {
@@ -10,13 +10,34 @@ const binarySearch = (data, from, to, value) => {
         }
 
         if (data[mid] > value) {
-            return binarySearch(data, from, mid - 1, value);
+            return recursiveBinarySearch(data, from, mid - 1, value);
         } else {
-            return binarySearch(data, mid + 1, to, value);
+            return recursiveBinarySearch(data, mid + 1, to, value);
         }
     }
 
     return -1;
 };
 
-console.log(binarySearch(list, 0, list.length - 1, 7));
+const iterativeBinarySearch = (data, value) => {
+    var from = 0;
+    var to = list.length - 1;
+    while (to >= from) {
+        var mid = Math.floor(from + (to - from) / 2);
+        if (data[mid] === value) {
+            return mid;
+        }
+
+        if (data[mid] > value) {
+            to = mid - 1;
+        } else {
+            from = mid + 1;
+        }
+    }
+
+    return -1;
+};
+
+console.log(recursiveBinarySearch(list, 0, list.length - 1, 7));
+
+console.log(iterativeBinarySearch(list, 7));
